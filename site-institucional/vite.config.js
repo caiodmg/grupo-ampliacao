76bac1b5277f9e08a81d50ad2b8a521c8a1ca9d3
caio <115330991+caiodmg@users.noreply.github.com>
2026-02-20 15:33:13 -0300

    Fix deployment configuration in vite.config.js

--- site-institucional/vite.config.js
+++ site-institucional/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// deploy fix
 export default defineConfig({
   base: '/grupo-ampliacao/',
   plugins: [react()],
-}) 
+})
